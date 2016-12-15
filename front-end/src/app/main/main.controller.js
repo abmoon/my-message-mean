32cb98714469650ed062fc97cc19c6b997713b1f
abmoon <avraham.luna@gmail.com>
2016-12-15 15:12:37 -0800

    Send a post from Angular to the Endpoint

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -1,12 +1,19 @@
 export class MainController {
   constructor ($http) {
     'ngInject';
-
+        this.$http = $http;
 
   }
 
-    postMessage() {
-        console.log("post");
+  postMessage() {
+        this.$http.post('http://localhost:5000/api/message', {msg: this.message});
+
     }
 
 }
+
+
+
+
+
+
